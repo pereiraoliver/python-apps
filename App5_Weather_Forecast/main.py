50bc2165f5e85fd38248eeb5b01f4232d76abea7
Oliver Pereira <pereira.oliver@gmail.com>
2026-08-22 18:07:29 +0530

    App5: Completed

--- App5_Weather_Forecast/main.py
+++ App5_Weather_Forecast/main.py
@@ -1,7 +1,8 @@
+import backend
 import plotly.express as px
 import streamlit as st
-from backend import get_data
 
+# Add title, text input, slider, select box, and sub header
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider(
@@ -12,11 +13,38 @@ days = st.slider(
 )
 
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
-
 st.subheader(f"{option} for the next {days} days in {place}")
-
-d, t = get_data(place, days, option)
-
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temerature (C)"})
-
-st.plotly_chart(figure)
+# Get the temperature/sky data
+try:
+    if place:
+        filtered_data = backend.get_data(place, days)
+        match option:
+            case "Temperature":
+                temperature = [
+                    float(dict["main"]["temp"]) / 10 for dict in filtered_data
+                ]
+                date = [dict["dt_txt"] for dict in filtered_data]
+                # Create a temperatureplot
+                figure = px.line(
+                    x=date, y=temperature, labels={"x": "Date", "y": "Temerature (C)"}
+                )
+                st.plotly_chart(figure)
+            case "Sky":
+                sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+                date = [dict["dt_txt"] for dict in filtered_data]
+                images = {
+                    "Clouds": "downloads/cloud.png",
+                    "Rain": "downloads/rain.png",
+                    "Clear": "downloads/clear.png",
+                    "Snow": "downloads/snow.png",
+                }
+                for i in range(0, len(sky_conditions), 6):
+                    cols = st.columns(6)
+                    for col, image, dt in zip(
+                        cols, sky_conditions[i : i + 6], date[i : i + 6]
+                    ):
+                        with col:
+                            st.image(images[image], use_container_width=True)
+                            st.write(dt)
+except KeyError:
+    st.write("That place does not exist. Try Again!")
